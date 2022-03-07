1eb7f4b784dcf248d24d1ef289785481fdcf8ddb
Jo Hanna Pearce <jo@nrwl.io>
2022-03-07 13:57:24 +0000

    random changes

--- apps/todos/src/app/app.component.ts
+++ apps/todos/src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
 import { Message } from '@todos/api-interfaces';
+
+// something different to make this affected
 
 @Component({
   selector: 'todos-root',
